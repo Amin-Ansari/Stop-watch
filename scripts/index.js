d40f78d62a78535413cd632abec94be1aeca3725
Amin Ansari <devansari@yahoo.com>
2022-04-26 15:01:26 +0430

    the first phase is done

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,25 @@ function timeShow() {
   timerElement.innerHTML = timeList.join(" : ");
 }
 
+function seccondAdd() {
+  timeList[2] = Number(timeList[2]) + 1;
+  timeShow();
+}
+function minuteAdd() {
+  if (Number(timeList[2]) == 60) {
+    timeList[2] = "00";
+    timeList[1] = Number(timeList[1]) + 1;
+    timeShow();
+  }
+}
+function hourAdd() {
+  if (Number(timeList[1]) == 60) {
+    timeList[1] = "00";
+    timeList[0] = Number(timeList[0]) + 1;
+    timeShow();
+  }
+}
 
+buttons[0].addEventListener("click", setInterval(seccondAdd, 1000));
 
-timeShow();
+timerElement.addEventListener("change", () => alert("done"));
