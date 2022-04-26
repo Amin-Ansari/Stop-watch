efccb320b4c78d6216ecc4b3d1da6123afa70902
Amin Ansari <devansari@yahoo.com>
2022-04-26 16:47:42 +0430

    The stop watch is compelete

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 let timerElement = document.querySelector(".timer");
 const buttons = document.querySelectorAll("button");
 let timeList = ["00", "00", "00"];
+var num = 0;
 var timeInterval;
 
 function timeShow() {
@@ -14,16 +15,37 @@ function timeShow() {
 }
 
 function seccondAdd() {
-  timeList[2] = Number(timeList[2]) + 1;
+  num = 0;
+  if (timeList[2] < 9) {
+    num = Number(timeList[2]) + 1;
+    timeList[2] = "0" + num;
+  } else {
+    num = Number(timeList[2]) + 1;
+    timeList[2] = num;
+  }
   timeShow();
 }
 function minuteAdd() {
   timeList[2] = "00";
-  timeList[1] = Number(timeList[1]) + 1;
+  num = 0;
+  if (timeList[1] < 9) {
+    num = Number(timeList[1]) + 1;
+    timeList[1] = "0" + num;
+  } else {
+    num = Number(timeList[1]) + 1;
+    timeList[1] = num;
+  }
 }
 function hourAdd() {
   timeList[1] = "00";
-  timeList[0] = Number(timeList[0]) + 1;
+  num = 0;
+  if (timeList[0] < 9) {
+    num = Number(timeList[0]) + 1;
+    timeList[0] = "0" + num;
+  } else {
+    num = Number(timeList[0]) + 1;
+    timeList[0] = num;
+  }
 }
 
 buttons[0].addEventListener("click", function () {
